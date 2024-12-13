350ca789cadaa01a0568203e7ec67e13df46d924
Katherine Whitlock <kate@skylinesynths.nyc>
2024-12-12 21:16:38 -0500

    Format CMake configs

--- Source/configDsp.cmake
+++ Source/configDsp.cmake
@@ -1,61 +1,57 @@
 function(configDsp project)
+  if(HOST)
+    target_compile_definitions(${project} PUBLIC __GNUC_PYTHON__)
+  endif()
 
-if (HOST)
-      target_compile_definitions(${project} PUBLIC __GNUC_PYTHON__)
-endif()
+  if(LOOPUNROLL)
+    target_compile_definitions(${project} PRIVATE ARM_MATH_LOOPUNROLL)
+  endif()
 
-if (LOOPUNROLL)
-  target_compile_definitions(${project} PRIVATE ARM_MATH_LOOPUNROLL)
-endif()
+  if(ROUNDING)
+    target_compile_definitions(${project} PRIVATE ARM_MATH_ROUNDING)
+  endif()
 
-if (ROUNDING)
-  target_compile_definitions(${project} PRIVATE ARM_MATH_ROUNDING)
-endif()
+  if(MATRIXCHECK)
+    target_compile_definitions(${project} PRIVATE ARM_MATH_MATRIX_CHECK)
+  endif()
 
-if (MATRIXCHECK)
-  target_compile_definitions(${project} PRIVATE ARM_MATH_MATRIX_CHECK)
-endif()
+  if(AUTOVECTORIZE)
+    target_compile_definitions(${project} PRIVATE ARM_MATH_AUTOVECTORIZE)
+  endif()
 
-if (AUTOVECTORIZE)
-    target_compile_definitions(${project} PRIVATE ARM_MATH_AUTOVECTORIZE) 
-endif()
-
-if (NEON)
+  if(NEON)
     # Used in arm_vec_math.h
-    target_include_directories(${project} PUBLIC "${DSP}/ComputeLibrary/Include")
-    target_include_directories(${project} PRIVATE "${DSP}/Ne10")
+    target_include_directories(${project} PUBLIC ${DSP}/ComputeLibrary/Include)
+    target_include_directories(${project} PRIVATE ${DSP}/Ne10)
     target_compile_definitions(${project} PUBLIC ARM_MATH_NEON)
+  endif()
 
-endif()
-
-if (NEONEXPERIMENTAL)
+  if(NEONEXPERIMENTAL)
     # Used in arm_vec_math.h
-    target_include_directories(${project} PUBLIC "${DSP}/ComputeLibrary/Include")
-    target_include_directories(${project} PRIVATE "${DSP}/Ne10")
+    target_include_directories(${project} PUBLIC ${DSP}/ComputeLibrary/Include)
+    target_include_directories(${project} PRIVATE ${DSP}/Ne10)
     target_compile_definitions(${project} PUBLIC ARM_MATH_NEON_EXPERIMENTAL)
-endif()
+  endif()
 
-if (MVEFLOAT16)
-    target_compile_definitions(${project} PRIVATE ARM_MATH_MVE_FLOAT16) 
-endif()
+  if(MVEFLOAT16)
+    target_compile_definitions(${project} PRIVATE ARM_MATH_MVE_FLOAT16)
+  endif()
 
-target_include_directories(${project} PRIVATE "${DSP}/PrivateInclude")
+  target_include_directories(${project} PRIVATE ${DSP}/PrivateInclude)
 
-if (MVEI OR MVEF OR HELIUM)
+  if(MVEI OR MVEF OR HELIUM)
     # By default, GCC does not enable implicit conversion between vectors of different numbers or types of elements
     # which is required by some code in CMSIS-DSP
-    if (LAXVECTORCONVERSIONS)
-       target_compile_options(${project} PRIVATE $<$<STREQUAL:${CMAKE_C_COMPILER_ID},GNU>:-flax-vector-conversions>)
-       target_compile_options(${project} PRIVATE $<$<STREQUAL:${CMAKE_C_COMPILER_ID},ARMClang>:-flax-vector-conversions=integer>)
+    if(LAXVECTORCONVERSIONS)
+      target_compile_options(${project} PRIVATE $<$<STREQUAL:${CMAKE_C_COMPILER_ID},GNU>:-flax-vector-conversions>)
+      target_compile_options(${project} PRIVATE $<$<STREQUAL:${CMAKE_C_COMPILER_ID},ARMClang>:-flax-vector-conversions=integer>)
     else()
-       target_compile_options(${project} PRIVATE $<$<STREQUAL:${CMAKE_C_COMPILER_ID},GNU>:-fno-lax-vector-conversions>)
-       target_compile_options(${project} PRIVATE $<$<STREQUAL:${CMAKE_C_COMPILER_ID},ARMClang>:-flax-vector-conversions=none>)
+      target_compile_options(${project} PRIVATE $<$<STREQUAL:${CMAKE_C_COMPILER_ID},GNU>:-fno-lax-vector-conversions>)
+      target_compile_options(${project} PRIVATE $<$<STREQUAL:${CMAKE_C_COMPILER_ID},ARMClang>:-flax-vector-conversions=none>)
     endif()
-endif()
+  endif()
 
-if (DISABLEFLOAT16)
-    target_compile_definitions(${project} PRIVATE DISABLEFLOAT16) 
-endif()
-
-
+  if(DISABLEFLOAT16)
+    target_compile_definitions(${project} PRIVATE DISABLEFLOAT16)
+  endif()
 endfunction()
